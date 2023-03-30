c3e4a7bbb142c624b57ca0e07ae470ead5dbf2e0
neusfer <125355473+neusfer@users.noreply.github.com>
2023-03-30 09:31:32 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("hola")
-    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.EigthNote)
     basic.pause(200)
     basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
-radio.setGroup(132)
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("hola")
+    basic.showIcon(IconNames.EigthNote)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    radio.sendString("hola")
+    basic.showIcon(IconNames.EigthNote)
+    basic.pause(200)
+    basic.clearScreen()
+})
+radio.setGroup(111)
